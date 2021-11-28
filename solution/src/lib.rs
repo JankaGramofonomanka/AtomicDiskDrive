d6727bfce8327f5e0fbbb36d7982e1b18c5e3f27
JankaGramofonomanka <tsrn666@gmail.com>
2021-11-28 14:01:02 +0100

    First commit

---
+++ solution/src/lib.rs
@@ -0,0 +1,141 @@
+mod domain;
+
+pub use crate::domain::*;
+pub use atomic_register_public::*;
+pub use register_client_public::*;
+pub use sectors_manager_public::*;
+pub use stable_storage_public::*;
+pub use transfer_public::*;
+
+pub async fn run_register_process(config: Configuration) {
+    unimplemented!()
+}
+
+pub mod atomic_register_public {
+    use crate::{
+        ClientRegisterCommand, OperationComplete, RegisterClient, SectorsManager, StableStorage,
+        SystemRegisterCommand,
+    };
+    use std::future::Future;
+    use std::pin::Pin;
+    use std::sync::Arc;
+
+    #[async_trait::async_trait]
+    pub trait AtomicRegister: Send + Sync {
+        /// Send client command to the register. After it is completed, we expect
+        /// callback to be called. Note that completion of client command happens after
+        /// delivery of multiple system commands to the register, as the algorithm specifies.
+        async fn client_command(
+            &mut self,
+            cmd: ClientRegisterCommand,
+            operation_complete: Box<
+                dyn FnOnce(OperationComplete) -> Pin<Box<dyn Future<Output = ()> + Send>>
+                    + Send
+                    + Sync,
+            >,
+        );
+
+        /// Send system command to the register.
+        async fn system_command(&mut self, cmd: SystemRegisterCommand);
+    }
+
+    /// Idents are numbered starting at 1 (up to the number of processes in the system).
+    /// Storage for atomic register algorithm data is separated into StableStorage.
+    /// Communication with other processes of the system is to be done by register_client.
+    /// And sectors must be stored in the sectors_manager instance.
+    pub async fn build_atomic_register(
+        self_ident: u8,
+        metadata: Box<dyn StableStorage>,
+        register_client: Arc<dyn RegisterClient>,
+        sectors_manager: Arc<dyn SectorsManager>,
+        processes_count: usize,
+    ) -> Box<dyn AtomicRegister> {
+        unimplemented!()
+    }
+}
+
+pub mod sectors_manager_public {
+    use crate::{SectorIdx, SectorVec};
+    use std::path::PathBuf;
+    use std::sync::Arc;
+
+    #[async_trait::async_trait]
+    pub trait SectorsManager: Send + Sync {
+        /// Returns 4096 bytes of sector data by index.
+        async fn read_data(&self, idx: SectorIdx) -> SectorVec;
+
+        /// Returns timestamp and write rank of the process which has saved this data.
+        /// Timestamps and ranks are relevant for atomic register algorithm, and are described
+        /// there.
+        async fn read_metadata(&self, idx: SectorIdx) -> (u64, u8);
+
+        /// Writes a new data, along with timestamp and write rank to some sector.
+        async fn write(&self, idx: SectorIdx, sector: &(SectorVec, u64, u8));
+    }
+
+    /// Path parameter points to a directory to which this method has exclusive access.
+    pub fn build_sectors_manager(path: PathBuf) -> Arc<dyn SectorsManager> {
+        unimplemented!()
+    }
+}
+
+pub mod transfer_public {
+    use crate::RegisterCommand;
+    use std::io::Error;
+    use tokio::io::{AsyncRead, AsyncWrite};
+
+    pub async fn deserialize_register_command(
+        data: &mut (dyn AsyncRead + Send + Unpin),
+        hmac_system_key: &[u8; 64],
+        hmac_client_key: &[u8; 32],
+    ) -> Result<(RegisterCommand, bool), Error> {
+        unimplemented!()
+    }
+
+    pub async fn serialize_register_command(
+        cmd: &RegisterCommand,
+        writer: &mut (dyn AsyncWrite + Send + Unpin),
+        hmac_key: &[u8],
+    ) -> Result<(), Error> {
+        unimplemented!()
+    }
+}
+
+pub mod register_client_public {
+    use crate::SystemRegisterCommand;
+    use std::sync::Arc;
+
+    #[async_trait::async_trait]
+    /// We do not need any public implementation of this trait. It is there for use
+    /// in AtomicRegister. In our opinion it is a safe bet to say some structure of
+    /// this kind must appear in your solution.
+    pub trait RegisterClient: core::marker::Send + core::marker::Sync {
+        /// Sends a system message to a single process.
+        async fn send(&self, msg: Send);
+
+        /// Broadcasts a system message to all processes in the system, including self.
+        async fn broadcast(&self, msg: Broadcast);
+    }
+
+    pub struct Broadcast {
+        pub cmd: Arc<SystemRegisterCommand>,
+    }
+
+    pub struct Send {
+        pub cmd: Arc<SystemRegisterCommand>,
+        /// Identifier of the target process. Those start at 1.
+        pub target: usize,
+    }
+}
+
+pub mod stable_storage_public {
+    #[async_trait::async_trait]
+    /// A helper trait for small amount of durable metadata needed by the register algorithm
+    /// itself. Again, it is only for AtomicRegister definition. StableStorage in unit tests
+    /// is durable, as one could expect.
+    pub trait StableStorage: Send + Sync {
+        async fn put(&mut self, key: &str, value: &[u8]) -> Result<(), String>;
+
+        async fn get(&self, key: &str) -> Option<Vec<u8>>;
+    }
+}
