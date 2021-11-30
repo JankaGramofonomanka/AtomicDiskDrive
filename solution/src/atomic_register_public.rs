13f074df764474753868c6e1a061140c5e4d044f
JankaGramofonomanka <tsrn666@gmail.com>
2021-11-30 16:11:22 +0100

    Add comments with the original algorithm

--- solution/src/atomic_register_public.rs
+++ solution/src/atomic_register_public.rs
@@ -141,6 +141,12 @@ impl ARModule {
         self.register_client.send(msg).await;
     }
 
+
+
+    /*
+    upon event < sbeb, Deliver | p [READ_PROC, r] > do
+        trigger < sl, Send | p, [VALUE, r, ts, wr, val] >;
+    */
     async fn read_proc(&self, cmd_header: SystemCommandHeader) {
         let (ts, wr) = self.sectors_manager.read_metadata(cmd_header.sector_idx).await;
         let val = self.sectors_manager.read_data(cmd_header.sector_idx).await;
@@ -154,6 +160,13 @@ impl ARModule {
         self.respond(cmd_header, content).await
     }
 
+    /*
+    upon event < sbeb, Deliver | p, [WRITE_PROC, r, ts', wr', v'] > do
+        if (ts', wr') > (ts, wr) then
+            (ts, wr, val) := (ts', wr', v');
+            store(ts, wr, val);
+        trigger < sl, Send | p, [ACK, r] >;
+    */
     async fn write_proc(
         &self, 
         cmd_header:     SystemCommandHeader,
@@ -171,6 +184,22 @@ impl ARModule {
         self.respond(cmd_header, SystemRegisterCommandContent::Ack).await
     }
 
+    /*
+    upon event <sl, Deliver | q, [VALUE, r, ts', wr', v'] > such that r == rid and !write_phase do
+        readlist[q] := (ts', wr', v');
+        if #(readlist) > N / 2 and (reading or writing) then
+            readlist[self] := (ts, wr, val);
+            (maxts, rr, readval) := highest(readlist);
+            readlist := [ _ ] `of length` N;
+            acklist := [ _ ] `of length` N;
+            write_phase := TRUE;
+            if reading = TRUE then
+                trigger < sbeb, Broadcast | [WRITE_PROC, rid, maxts, rr, readval] >;
+            else
+                (ts, wr, val) := (maxts + 1, rank(self), writeval);
+                store(ts, wr, val);
+                trigger < sbeb, Broadcast | [WRITE_PROC, rid, maxts + 1, rank(self), writeval] >;
+    */
     async fn value(
         &mut self, 
         cmd_header:     SystemCommandHeader,
@@ -231,6 +260,67 @@ impl ARModule {
             }
         }
     }
+
+    /* 
+    upon event < nnar, Init > do
+        (ts, wr, val) := (0, 0, _);
+        rid:= 0;
+        readlist := [ _ ] `of length` N;
+        acklist := [ _ ] `of length` N;
+        reading := FALSE;
+        writing := FALSE;
+        writeval := _;
+        readval := _;
+        write_phase := FALSE;
+        store(wr, ts, val, rid);
+    */
+
+    /*
+    upon event < nnar, Recovery > do
+        retrieve(wr, ts, val, rid);
+        readlist := [ _ ] `of length` N;
+        acklist := [ _ ]  `of length` N;
+        reading := FALSE;
+        readval := _;
+        write_phase := FALSE;
+        writing := FALSE;
+        writeval := _;
+    */
+
+    /*
+    upon event < nnar, Read > do
+        rid := rid + 1;
+        store(rid);
+        readlist := [ _ ] `of length` N;
+        acklist := [ _ ] `of length` N;
+        reading := TRUE;
+        trigger < sbeb, Broadcast | [READ_PROC, rid] >;
+    */
+
+    /*
+    upon event < nnar, Write | v > do
+        rid := rid + 1;
+        writeval := v;
+        acklist := [ _ ] `of length` N;
+        readlist := [ _ ] `of length` N;
+        writing := TRUE;
+        store(rid);
+        trigger < sbeb, Broadcast | [READ_PROC, rid] >;
+    */
+
+    /*
+    upon event < sl, Deliver | q, [ACK, r] > such that r == rid and write_phase do
+        acklist[q] := Ack;
+        if #(acklist) > N / 2 and (reading or writing) then
+            acklist := [ _ ] `of length` N;
+            write_phase := FALSE;
+            if reading = TRUE then
+                reading := FALSE;
+                trigger < nnar, ReadReturn | readval >;
+            else
+                writing := FALSE;
+                trigger < nnar, WriteReturn >;
+    */
 }
 
 
