d5f1ddc8e02c77527ea502f1ce81f22787cfbfe1
JankaGramofonomanka <tsrn666@gmail.com>
2021-12-05 13:32:10 +0100

    Implement `RegisterClient`

--- solution/src/register_client_public.rs
+++ solution/src/register_client_public.rs
@@ -2,6 +2,13 @@
 use crate::SystemRegisterCommand;
 use std::sync::Arc;
 
+use std::collections::HashMap;
+use tokio::net::TcpStream;
+use crate::transfer_public::serialize_system_command;
+use crate::RegisterCommand;
+use tokio::task::JoinHandle;
+
+
 #[async_trait::async_trait]
 /// We do not need any public implementation of this trait. It is there for use
 /// in AtomicRegister. In our opinion it is a safe bet to say some structure of
@@ -24,3 +31,82 @@ pub struct Send {
     pub target: usize,
 }
 
+
+
+struct RCModule {
+    tcp_connections: HashMap<usize, (String, u16)>,
+    hmac_key: [u8; 64],
+}
+
+impl RCModule {
+    fn new(tcp_connections: Vec<(String, u16)>, hmac_key: [u8; 64]) -> Self {
+        
+        let mut tcp_conns = HashMap::new();
+
+        let mut proc_id: usize = 1;
+        for (host, port) in tcp_connections {
+
+            tcp_conns.insert(proc_id, (host, port));
+
+            proc_id += 1;
+        }
+
+        RCModule {
+            tcp_connections: tcp_conns,
+            hmac_key: hmac_key,
+        }
+    }
+
+}
+
+
+async fn send_msg(
+    cmd: Arc<SystemRegisterCommand>, 
+    host: String, 
+    port: u16, 
+    hmac_key: [u8; 64]
+) {
+
+    let address = format!("{}:{}", host, port);
+
+    // TODO: how to handle errors?
+    let mut stream = TcpStream::connect(address).await.unwrap();
+
+    // TODO: how to handle errors?
+    serialize_system_command(&cmd, &mut stream, &hmac_key).await.unwrap();
+
+}
+
+#[async_trait::async_trait]
+impl RegisterClient for RCModule {
+    /// Sends a system message to a single process.
+    async fn send(&self, msg: Send) {
+        
+        let (host, port) = &self.tcp_connections[&msg.target];
+        send_msg(msg.cmd, host.clone(), *port, self.hmac_key).await
+
+    }
+
+    /// Broadcasts a system message to all processes in the system, including self.
+    async fn broadcast(&self, msg: Broadcast) {
+
+        let mut send_handles: Vec<JoinHandle<()>> = vec![];
+        for (_, (host, port)) in self.tcp_connections.iter() {
+            
+            let handle = tokio::spawn(send_msg(
+                msg.cmd.clone(),
+                host.clone(),
+                port.clone(),
+                self.hmac_key,
+            ));
+
+            send_handles.push(handle);
+        }
+
+        for handle in send_handles.drain(..) {
+
+            // TODO: errors?
+            handle.await.unwrap();
+        }
+    }
+}
