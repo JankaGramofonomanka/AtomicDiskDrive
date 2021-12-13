815024eae27a7c68445d0d8fdd052285d756abc9
JankaGramofonomanka <tsrn666@gmail.com>
2021-12-13 15:24:14 +0100

    Refactor solution/src/lib.rs

--- solution/src/lib.rs
+++ solution/src/lib.rs
@@ -34,190 +34,200 @@ pub async fn run_register_process(config: Configuration) {
     let address = format!("{}:{}", self_host, self_port);
     let listener = TcpListener::bind(address).await.unwrap();
 
-    let atomic_register = Arc::new(Mutex::new(build_ar(&config).await));
+    //let atomic_register = Arc::new(Mutex::new(RegisterProcess::build_ar(&config).await));
     
+    let register_process = RegisterProcess::new(config).await;
     loop {
         
         let (stream, _) = listener.accept().await.unwrap();
         let (read_stream, write_stream) = stream.into_split();
 
         tokio::spawn(
-            handle_stream(
+            register_process.clone().handle_stream(
                 Arc::new(Mutex::new(read_stream)),
                 Arc::new(Mutex::new(write_stream)),
-                atomic_register.clone(),
-                config.hmac_system_key,
-                config.hmac_client_key,
-                config.public.max_sector,
             )
         );
     }
 
 }
 
+struct RegisterProcess {
+    config: Configuration,
+    atomic_register: Arc<Mutex<ARModule>>,
+}
 
+impl RegisterProcess {
 
-async fn handle_stream<'a>(
-    read_stream_ref: Arc<Mutex<OwnedReadHalf>>, 
-    write_stream_ref: Arc<Mutex<OwnedWriteHalf>>, 
-    atomic_register: Arc<Mutex<dyn AtomicRegister>>,
-    hmac_system_key: [u8; 64],
-    hmac_client_key: [u8; 32],
-    max_sector: u64,
-) {
-    loop {
+    async fn new(config: Configuration) -> Arc<Self> {
+        let ar = RegisterProcess::build_ar(&config).await;
         
-        let cmd;
-        let valid;
+        Arc::new(
+            RegisterProcess{
+                config: config,
+                atomic_register: Arc::new(Mutex::new(ar)),
+            }
+        )
+    }
 
-        {
-            let mut stream_guard = read_stream_ref.lock().await;
-            let stream = stream_guard.deref_mut();
-            let res = deserialize_register_command(
-                &mut *stream,
-                &hmac_system_key,
-                &hmac_client_key,
-            ).await;
+    async fn handle_stream<'a>(
+        self: Arc<Self>, 
+        read_stream_ref: Arc<Mutex<OwnedReadHalf>>, 
+        write_stream_ref: Arc<Mutex<OwnedWriteHalf>>, 
+    ) {
+        loop {
+            
+            let cmd;
+            let valid;
 
-            match res {
+            {
+                let mut stream_guard = read_stream_ref.lock().await;
+                let stream = stream_guard.deref_mut();
+                let res = deserialize_register_command(
+                    &mut *stream,
+                    &self.config.hmac_system_key,
+                    &self.config.hmac_client_key,
+                ).await;
 
-                Ok((c, v)) => {
-                    cmd = c;
-                    valid = v;
-                }
-                
-                Err(e) => {
-                    if e.kind() == ErrorKind::UnexpectedEof {
-                        break; 
+                match res {
+
+                    Ok((c, v)) => {
+                        cmd = c;
+                        valid = v;
                     }
-                    continue;
-                },
-            }
-        }
-
-        // invalid tag --------------------------------------------------------
-        if !valid {
-            match &cmd {
-                // TODO: what in case of system commands?
-                RegisterCommand::System(_) => {},
-
-                RegisterCommand::Client(cmd) => {
-                    let result = invalid_result(cmd, StatusCode::AuthFailure);
                     
-                    let mut stream_guard = write_stream_ref.lock().await;
-                    let stream = stream_guard.deref_mut();
-                    serialize_response(&result, &mut *stream, &hmac_client_key).await.unwrap();
+                    Err(e) => {
+                        if e.kind() == ErrorKind::UnexpectedEof {
+                            break; 
+                        }
+                        continue;
+                    },
                 }
             }
-            
-            return;
-        }
 
-        // invalid sector id --------------------------------------------------
-        let sector_idx = get_sector_idx(&cmd);
-        if sector_idx > max_sector {
-            match &cmd {
-                // TODO: what in case of system commands?
-                RegisterCommand::System(_) => {},
+            // invalid tag --------------------------------------------------------
+            if !valid {
+                match &cmd {
+                    // TODO: what in case of system commands?
+                    RegisterCommand::System(_) => {},
 
-                RegisterCommand::Client(cmd) => {
-                    let result = invalid_result(cmd, StatusCode::InvalidSectorIndex);
-                    
-                    let mut stream_guard = write_stream_ref.lock().await;
-                    let stream = stream_guard.deref_mut();
-                    serialize_response(&result, &mut *stream, &hmac_client_key).await.unwrap();
-                }
-            }
-            
-            return;
-        }
-
-
-        // handling -----------------------------------------------------------
-        match cmd {
-            RegisterCommand::System(cmd) => {
-                
-                let mut ar = atomic_register.lock().await;
-                ar.system_command(cmd).await;
-            },
-            
-            RegisterCommand::Client(cmd) => {
-                
-                let hmac_key = hmac_client_key.clone();
-                let write_stream_ref_clone = write_stream_ref.clone();
-                let operation_complete: Box<
-                    dyn FnOnce(OperationComplete) 
-                            -> Pin<Box<dyn Future<Output = ()> + core::marker::Send>>
-                        + core::marker::Send
-                        + core::marker::Sync,
-                > = Box::new(move |result| {
-                    Box::pin(async move {
+                    RegisterCommand::Client(cmd) => {
+                        let result = invalid_result(cmd, StatusCode::AuthFailure);
                         
-                        let mut stream_guard = write_stream_ref_clone.lock().await;
-                        
+                        let mut stream_guard = write_stream_ref.lock().await;
                         let stream = stream_guard.deref_mut();
-                        serialize_response(&result, &mut *stream, &hmac_key).await.unwrap();
-                    })
-                });
+                        serialize_response(&result, &mut *stream, &self.config.hmac_client_key).await.unwrap();
+                    }
+                }
+                
+                return;
+            }
 
-                let mut ar = atomic_register.lock().await;
-                ar.client_command(cmd, operation_complete).await;
-            },
-        };
+            // invalid sector id --------------------------------------------------
+            let sector_idx = get_sector_idx(&cmd);
+            if sector_idx > self.config.public.max_sector {
+                match &cmd {
+                    // TODO: what in case of system commands?
+                    RegisterCommand::System(_) => {},
 
-    } 
+                    RegisterCommand::Client(cmd) => {
+                        let result = invalid_result(cmd, StatusCode::InvalidSectorIndex);
+                        
+                        let mut stream_guard = write_stream_ref.lock().await;
+                        let stream = stream_guard.deref_mut();
+                        serialize_response(&result, &mut *stream, &self.config.hmac_client_key).await.unwrap();
+                    }
+                }
+                
+                return;
+            }
 
-}
 
+            // handling -----------------------------------------------------------
+            match cmd {
+                RegisterCommand::System(cmd) => {
+                    
+                    let mut ar = self.atomic_register.lock().await;
+                    ar.system_command(cmd).await;
+                },
+                
+                RegisterCommand::Client(cmd) => {
+                    
+                    let hmac_key = self.config.hmac_client_key.clone();
+                    let write_stream_ref_clone = write_stream_ref.clone();
+                    let operation_complete: Box<
+                        dyn FnOnce(OperationComplete) 
+                                -> Pin<Box<dyn Future<Output = ()> + core::marker::Send>>
+                            + core::marker::Send
+                            + core::marker::Sync,
+                    > = Box::new(move |result| {
+                        Box::pin(async move {
+                            
+                            let mut stream_guard = write_stream_ref_clone.lock().await;
+                            
+                            let stream = stream_guard.deref_mut();
+                            serialize_response(&result, &mut *stream, &hmac_key).await.unwrap();
+                        })
+                    });
 
+                    let mut ar = self.atomic_register.lock().await;
+                    ar.client_command(cmd, operation_complete).await;
+                },
+            };
 
+        } 
 
-async fn build_ar(config: &Configuration)
-    -> ARModule {
-
-    // TODO: what to do with the unwraps?
-
-    let processes_count = config.public.tcp_locations.len();
-
-    // build register client
-    let register_client = Arc::new(
-        RCModule::new(config.public.tcp_locations.clone(), config.hmac_system_key)
-    );
-
-    // build sectors manager
-    let mut sectors_dir = config.public.storage_dir.clone();
-    sectors_dir.push("sectors");
-    if !sectors_dir.is_dir() {
-        create_dir(sectors_dir.clone()).await.unwrap();
     }
-    let sectors_manager = build_sectors_manager(sectors_dir);
-
-    let self_rank = config.public.self_rank;
 
 
 
-    // build stable sotrage
-    // TODO: should this be global or per atomic register
-    let mut metadata_dir = config.public.storage_dir.clone();
-    metadata_dir.push("ar_metadata");
-    if !metadata_dir.is_dir() {
-        create_dir(metadata_dir.clone()).await.unwrap();
+
+    async fn build_ar(config: &Configuration)
+        -> ARModule {
+
+        // TODO: what to do with the unwraps?
+
+        let processes_count = config.public.tcp_locations.len();
+
+        // build register client
+        let register_client = Arc::new(
+            RCModule::new(config.public.tcp_locations.clone(), config.hmac_system_key)
+        );
+
+        // build sectors manager
+        let mut sectors_dir = config.public.storage_dir.clone();
+        sectors_dir.push("sectors");
+        if !sectors_dir.is_dir() {
+            create_dir(sectors_dir.clone()).await.unwrap();
+        }
+        let sectors_manager = build_sectors_manager(sectors_dir);
+
+        let self_rank = config.public.self_rank;
+
+
+
+        // build stable sotrage
+        // TODO: should this be global or per atomic register
+        let mut metadata_dir = config.public.storage_dir.clone();
+        metadata_dir.push("ar_metadata");
+        if !metadata_dir.is_dir() {
+            create_dir(metadata_dir.clone()).await.unwrap();
+        }
+        let metadata = Storage::new(metadata_dir).await;
+        
+        let atomic_register = ARModule::new(
+            self_rank,
+            Box::new(metadata),
+            register_client.clone(),
+            sectors_manager.clone(),
+            processes_count,
+        ).await;
+
+
+        atomic_register
+
     }
-    let metadata = Storage::new(metadata_dir).await;
-    
-    let atomic_register = ARModule::new(
-        self_rank,
-        Box::new(metadata),
-        register_client.clone(),
-        sectors_manager.clone(),
-        processes_count,
-    ).await;
-
-
-    atomic_register
-
 }
-
 
 
 
