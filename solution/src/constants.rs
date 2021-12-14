39620a7164845170b351cc10bb7bf3d6d7126d62
JankaGramofonomanka <tsrn666@gmail.com>
2021-12-14 19:32:02 +0100

    Add comments to solution/src/constants.rs

--- solution/src/constants.rs
+++ solution/src/constants.rs
@@ -1,11 +1,11 @@
 
-
+// Pandding lengths
 pub const CLIENT_REQUEST_PADDING_LENGTH:    usize = 8 - 4 - 1;
 pub const CLIENT_RESPONSE_PADDING_LENGTH:   usize = 8 - 4 - 2;
 pub const SYSTEM_PADDING_LENGTH:            usize = 8 - 4 - 2;
 pub const READ_WRITE_PROC_PADDING_LENGTH:   usize = 8 - 1;
 
-
+// Message types
 pub const SECTOR_SIZE:              usize = 4096;
 pub const CONTENT_LENGTH:           usize = SECTOR_SIZE;
 pub const SYSTEM_CONTENT_LENGTH:    usize = CONTENT_LENGTH + 16;
@@ -24,4 +24,6 @@ pub const WRITE_RETURN_TYPE:    u8 = 0x40 + 0x02;
 pub const MAX_CMD_TYPE:         u8 = ACK_TYPE;
 pub const MIN_CMD_TYPE:         u8 = READ_TYPE;
 
-pub const NUM_REGISTERS:        u32 = 16;
+
+// Number of atomiuc register instances in a single process
+pub const NUM_REGISTERS: u32 = 16;
